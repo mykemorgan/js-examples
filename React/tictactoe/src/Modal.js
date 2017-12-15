57de32c9c90e33e3725de72c8e16f4158b95d3b8
Myke Morgan <myke_morgan@yahoo.com>
2017-12-15 13:18:46 -0800

    TTT: use react-transition-group to animate the modal appearance
    It's a little cleaner since <Transition> not handles all the timeouts
    and whatnot, but the styles are still hardcoded in the code. Need to
    update that so we can keep them in the CSS file.

--- React/tictactoe/src/Modal.js
+++ React/tictactoe/src/Modal.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 import './Modal.css';
 
 // A Modal message dialog.
 // Fade in animation when made visible.
 // Opaque-in the main window when made visible.
 
-// XXX/TODO Other things to look into for animating the modal:
-// https://reactcommunity.org/react-transition-group/
-// The above seems non-obvious for portals? But maybe with this new Portal around Modal scheme.
-//
-// Or, try this guy's example using react-motion:
+// Another animation example using react-motion:
 // https://github.com/touqeerkhan11/react-portal-example
 // Now in UIModal.js file
 
+
+// ----------------------------------------------------------------------
+// Transition definitions for using react transition groups from:
+// https://reactcommunity.org/react-transition-group/
+// XXX/TODO this should really be part of the component definition.
+const transitionDuration = 300;
+const defaultStyleModal = {
+    transitionDuration: `${transitionDuration}ms`,
+    transitionProperty: `opacity`,
+};
+const transitionStylesModal = {
+    entering: { opacity: 0 },
+    entered: { opacity: 1 },
+};
+
+const defaultStyleBackground = {
+    transitionDuration: `${transitionDuration}ms`,
+    transitionProperty: `background-color`,
+};
+const transitionStylesBackground = {
+    entering: { backgroundColor: `rgba(250, 250, 250, 0.85)` },
+    entered: { backgroundColor: `rgba(33, 33, 33, 0.85)` },
+};
+
+// Trying to divorce the styles of the content from the styles of the dialog,
+// to make the animation of the modal appearing more straightforward.
+// The only thing we're rendering directly here is:
+// - a "background" div to darken the main window behind the Modal.
+// - the actual modal dialog div, into which we stuff the children.
+//
 // @prop bool {open} - true to show the modal, false to hide
 // @prop string {header} - header text to show in the modal
 // @prop string {onClose} - callback to call when close() button pressed.
@@ -23,58 +50,50 @@ class Modal extends React.Component {
     }
     // See note in GameOver Component about difference between componentDidUpdate() here
     // and componentDidMount() there.
-    //
-    // Therefore if we set set an animation timeout it has to be:
-    // in DidMount in GameOver, called after render().
-    // in DidUpdate in Model, also called after render().
     componentDidUpdate() {
-        console.group(`Modal::componentDidUpdate()`);
-        // Don't do the open animation after the window closed.
-        // XXX/mm Do a close animation instead in the WillUpdate?
-        if (!this.props.open) {
-            console.debug(`Closing Modal: skipping animation`);
-            console.groupEnd(`Modal::componentDidUpdate()`);
-            return;
-        }
-
-        console.log(`Modal animation timeout launched DidUpdate()`);
-        // If we set set an animation timeout it has to be here.
-
-        // XXX/mm Fine for a test but we can't be having the Modal hard code the animation...
-        //        Should really call a callback to invoke to do the animation.
-        // XXX/mm Also, should be setting a named set of properties from .css file, not hard coding it.
-        //        Maybe this is where we need to use CSSTransition component?
-        setTimeout(() => {
-            console.log(`Modal timeout triggered after DidUpdate`);
-            // document.getElementById('modal-dialog').style.add('.modal-dialog-open');
-            // XXX/mm Getting dialog here with get by class feels like a hack. Probably should save it somehow?
-            let dialogs = document.getElementsByClassName('modal-dialog');
-            console.log(`Found ${dialogs.length} modal-dialogs`);
-            if (dialogs && dialogs[0]) {
-                dialogs[0].style.opacity = '1.0';
-            }
-
-            let backgrounds = document.getElementsByClassName('modal-background');
-            console.log(`Found ${backgrounds.length} modal-backgrounds`);
-            if (backgrounds && backgrounds[0]) {
-                backgrounds[0].style.backgroundColor = 'rgba(33, 33, 33, 0.85)';
-            }
-        }, 1);
-        console.groupEnd(`Modal::componentDidUpdate()`);
+        console.log(`Modal::componentDidUpdate()`);
     }
 
-    // Trying to divorce the styles of the content from the styles of the dialog,
-    // to make the animation of the modal appearing more straightforward.
-    // The only thing we're rendering directly here is:
-    // - a "background" div to darken the main window behind the Modal.
-    // - the actual modal dialog div, into which we stuff the children.
+    // A note on <Transition>'s prop's:
+    // the "appear" property says to apply the "enter" lifecycle transition on mount
+    // the "timeout" property governs how long between applying transitions "entering" and "entered"
+    // Since what we want is to apply the initial state (which has a CSS transition with duration),
+    // then immediately the end CSS state, we want the top level "timeout" property to be zero.
+    //
+    // <Transition> is thus just being used as a transparent state machine which we happen to want
+    // to apply state 1 (CSS initial) then state 2 immediately (CSS endpoint) and let CSS do the animation.
+    //
+    // XXX/TODO - Apply CSS styles via class names or something instead of constants in the code...
     render() {
         console.log(`Modal::render()`);
         return this.props.open ? (
-                <div>
-                <div className="modal-background" />
-                <div role="dialog" className="modal-dialog">{this.props.children}</div>
-                </div>
+            <div>
+            <Transition in={this.props.open} timeout={0} appear={true}>
+            {(state) => {
+                console.log(`Applying background transition state "${state}"`);
+                return (
+                    <div className="modal-background"
+                         style={{...defaultStyleBackground,
+                                 ...transitionStylesBackground[state]}}
+                    />
+                )}
+            }
+            </Transition>
+            <Transition in={this.props.open} timeout={0} appear={true}>
+            {(state) => {
+                console.log(`Applying modal transition state "${state}"`);
+                return (
+                    <div role="dialog"
+                         className="modal-dialog"
+                         style={{...defaultStyleModal,
+                                 ...transitionStylesModal[state]}}
+                    >
+                    {this.props.children}
+                    </div>
+                )}
+            }
+            </Transition>
+            </div>
         ) : null;
     }
 
